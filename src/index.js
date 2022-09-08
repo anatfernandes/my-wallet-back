d154f723cf17803620fe81713f241686ab350b1b
Ana Luiza Teixeira Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-08 16:24:36 -0300

    feat: add record in the path "/record"

--- src/index.js
+++ src/index.js
@@ -28,6 +28,19 @@ const userSchema = joi.object({
     password: joi.string().required()
 });
 
+const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+});
+
+const recordSchema = joi.object({
+    id: joi.number(),
+    date: joi.string(),
+    details: joi.string().required().max(20),
+    price: joi.string().required(),
+    type: joi.string().required()
+});
+
 
 server.post('/sign-up', async (req, res) => {
     const { name, email, password } = req.body;
@@ -78,6 +91,13 @@ server.post('/sign-up', async (req, res) => {
 server.post('/sign-in', async (req, res) => {
     const { email, password } = req.body;
 
+    const isValid = loginSchema.validate({ email, password }, { abortEarly: false });
+
+    if (isValid.error) {
+        const errors = isValid.error.details.map(({ message }) => message);
+        return res.status(400).send({ message:errors });
+    }
+
     let user;
 
     try {
@@ -101,6 +121,79 @@ server.post('/sign-in', async (req, res) => {
     }
 
     res.status(200).send({ email, token });
+});
+
+server.post('/record', async (req, res) => {
+    const token = req.headers.authorization?.replace('Bearer ');
+    const { details, price, type } = req.body;
+
+    let record = {
+        id: +new Date(),
+        date: new Date().toLocaleDateString('pt-br'),
+        details,
+        price,
+        type
+    };
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    const isValid = recordSchema.validate(record, { abortEarly: false });
+
+    if (isValid.error) {
+        const errors = isValid.error.details.map(({ message }) => message);
+        return res.status(400).send({ message:errors });
+    }
+
+    let session;
+
+    try {
+        session = await db.collection('sessions').findOne({ token });
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+
+    if (!session) {
+        return res.sendStatus(404);
+    }
+
+    let userRecords;
+
+    try {
+        userRecords = await db.collection('records').findOne({ user:session.userId });
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+
+    if (!userRecords) {
+        const newRecord = {
+            user:session.userId,
+            records:[record]
+        }
+
+        try {
+            db.collection('records').insertOne(newRecord);
+        } catch (error) {
+            console.log(error);
+            return res.sendStatus(500);
+        }
+    }
+    else {
+        try {
+            db.collection('records').updateOne(
+                { user:session.userId },
+                { $push: { records: record } }
+            );
+        } catch (error) {
+            console.log(error);
+            return res.sendStatus(500);
+        }
+    }
+
+    res.sendStatus(201);
 });
 
 
