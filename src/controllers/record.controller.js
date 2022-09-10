ef9ef29b16be91e5765bbe4ab354529033f05707
Ana Luiza Teixeira Fernandes <analuizateixeirafernandes184@gmail.com>
2022-09-10 17:27:01 -0300

    feat: delete record in the path "/records/:id"

--- src/controllers/record.controller.js
+++ src/controllers/record.controller.js
@@ -1,21 +1,11 @@
-import joi from 'joi';
 import mongo from '../database/db.js';
 
 const db = await mongo();
 
 
-
 async function createRecord (req, res) {
     const { session } = res.locals;
     const { details, price, type } = req.body;
-
-    const record = {
-        id: +new Date(),
-        date: new Date().toLocaleDateString('pt-br'),
-        details,
-        price,
-        type
-    };
 
     let userRecords;
 
@@ -25,6 +15,14 @@ async function createRecord (req, res) {
         console.log(error);
         return res.sendStatus(500);
     }
+
+    const record = {
+        id: userRecords.records.length + 1,
+        date: new Date().toLocaleDateString('pt-br'),
+        details,
+        price,
+        type
+    };
 
     try {
         db.collection('records').updateOne(
@@ -54,4 +52,34 @@ async function getRecords (req, res) {
     res.status(201).send(userRecords.records);
 }
 
-export { createRecord, getRecords };
+async function deleteRecord (req, res) {
+    const { session } = res.locals;
+    const { idRecord } = req.params;
+
+    let data;
+
+    try {
+        data = await db.collection('records').findOne({ user:session.userId });
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+
+    if (!data.records[idRecord]) return res.sendStatus(404);
+    
+    data.records.splice((idRecord - 1), 1);
+
+    try {
+        await db.collection('records').updateOne(
+            { user:session.userId },
+            { $set: { records: data.records } }
+        );
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500);
+    }
+
+    res.sendStatus(200);
+}
+
+export { createRecord, getRecords, deleteRecord };
--- src/routers/record.routers.js
+++ src/routers/record.routers.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createRecord, getRecords } from '../controllers/record.controller.js';
+import { createRecord, deleteRecord, getRecords } from '../controllers/record.controller.js';
 import hasUserMiddleware from '../middlewares/auth.middleware.js';
 import { recordSchemaMiddleware } from '../middlewares/schemas.middleware.js';
 
@@ -9,5 +9,6 @@ router.use(hasUserMiddleware);
 
 router.post('/record', recordSchemaMiddleware, createRecord);
 router.get('/records', getRecords);
+router.delete('/records/:idRecord', deleteRecord);
 
 export default router;
